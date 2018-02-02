1de9ae9596956d8bcbcafc340c7ad6c591005120
AndreasGassmann <andreas@andreasgassmann.ch>
2018-02-02 05:57:49 +0100

    docs(): update readme.md

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # translation-string-finder
 
+A simple package that checks if your translation strings are still used in your project. All it does is simple string-matching, so if your translation keys are common words it might not work as expected.
+
+Format of the translation file:
+
+```json
+{
+  "GLOBAL.GREETING": "Welcome!",
+  "GLOBAL.NAME": "Name"
+}
+```
+
+Basic usage:
+
+```javascript
+
+const checkTranslationStrings = require('./index');
+
+let projectPath = __dirname + '/test/test_project/';
+let translationFile = __dirname + '/test/test_project/translations/en.json';
+let ignoredDirectories = ['translations/', 'ignoredDir/'];
+let ignoredFiles = ['DS_Store', 'd.ts', 'ttf', 'woff', 'woff2', 'min.js', 'gif', 'jpg', 'jpeg', 'png', 'ico'];
+
+;(async () => {
+  let translationUsage = await checkTranslationStrings(projectPath, translationFile, ignoredDirectories, ignoredFiles);
+  console.log(translationUsage);
+});
+
+```
--- example.js
+++ example.js
@@ -1,19 +1,19 @@
+const checkTranslationStrings = require('./index');
+
 let projectPath = __dirname + '/test/test_project/';
 let translationFile = __dirname + '/test/test_project/translations/en.json';
 let ignoredDirectories = ['translations/', 'ignoredDir/'];
 let ignoredFiles = ['DS_Store', 'd.ts', 'ttf', 'woff', 'woff2', 'min.js', 'gif', 'jpg', 'jpeg', 'png', 'ico'];
 
-const checkTranslationStrings = require('./index');
-
 ;(async () => {
-  let stringResults = await checkTranslationStrings(projectPath, translationFile, ignoredDirectories, ignoredFiles);
-  let keys = Object.keys(stringResults);
+  let translationUsage = await checkTranslationStrings(projectPath, translationFile, ignoredDirectories, ignoredFiles);
+  let keys = Object.keys(translationUsage);
 
   console.log(`There are ${ keys.length } strings in your project.`);
-  console.log(`${ keys.filter(k => !stringResults[k] ).length } of them are unused:\n`);
+  console.log(`${ keys.filter(k => !translationUsage[k] ).length } of them are unused:\n`);
 
-  Object.keys(stringResults).forEach(s => { // Display result
-    if (!stringResults[s]) {
+  Object.keys(translationUsage).forEach(s => { // Display result
+    if (!translationUsage[s]) {
       console.log(s);
     }
   });
